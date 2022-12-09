22624cc9761e1cc9ac8ef9e5b1cb8391d1cf9a97
Jujuppi <juuso.saikkonen@hotmail.fi>
2022-12-09 14:52:07 +0200

    LookAtPlayer
    + NPC Looks at the player upon starting dialogue, and after the dialogue ends, as long as the player is still in its range

--- Assets/Scripts/NPCInteractable.cs
+++ Assets/Scripts/NPCInteractable.cs
@@ -4,8 +4,39 @@ using UnityEngine;
 
 public class NPCInteractable : MonoBehaviour
 {
+    public float PlayerLoseDistance = 5f;
+    private GameObject player;
+    private Quaternion OriginalRotation;
+
     public void Interact()
     {
         FindObjectOfType<DialogueTrigger>().TriggerDialogue();
+
+        player = FindObjectOfType<PlayerMovement>().gameObject;
+        OriginalRotation = transform.rotation;
+
+        CancelInvoke();
+        LookAtPlayer();
+    }
+
+    private void LookAtPlayer() // Katsoo pelaajaa päin kunnes hän lähtee alueelta
+    {
+        if((player.transform.position - transform.position).magnitude < PlayerLoseDistance) // Player is in range
+        {
+            // Kattoo pelaajaa päin, mutta ignooraa pelaajan korkeuden
+            transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z));
+            Invoke("LookAtPlayer", 0.1f);
+        }
+        else
+        {
+            transform.rotation = OriginalRotation;
+        }
+    }
+
+    [ExecuteInEditMode] // Pirtää PlayerLoseDistancen kokoisen pallon
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, PlayerLoseDistance);
     }
 }
